608fdaf0a0c2888559e0498af0f7615777589c52
Ross Kelso <rosskelso@btinternet.com>
2017-01-02 18:13:29 +0000

    This works locally

--- start.sh
+++ start.sh
@@ -1,1 +1,5 @@
-java -Dserver.port=$PORT $JAVA_OPTS -jar build/libs/Food-Alert-Server-0.1.0.jar $PORT
+cp libs/mysql-connector-java-5.1.6.jar build/libs/mysql-connector-java-5.1.6.jar
+cd build/libs/
+##export CLASSPATH=mysql-connector-java-5.1.6.jar:.
+
+java -Dserver.port=$PORT $JAVA_OPTS -jar Food-Alert-Server-0.1.0.jar $PORT
