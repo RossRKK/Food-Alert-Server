333920cce656b7646383970d046ac001f2601c1b
Ross Kelso <rosskelso@btinternet.com>
2017-01-02 18:37:09 +0000

    That was stupid

--- src/main/java/server/DatabaseManager.java
+++ src/main/java/server/DatabaseManager.java
@@ -39,7 +39,7 @@ public class DatabaseManager {
 	public String getJSON(String ean) throws SQLException {
 		// get the results of a query for the ean using prepared
 		// statements to make injection impossible
-		PreparedStatement stmt = con.prepareStatement("select * " + ConfigLoader.getFoodTableName() + " food where ean = ?");
+		PreparedStatement stmt = con.prepareStatement("select * " + ConfigLoader.getFoodTableName() + " where ean = ?");
 		stmt.setString(1, ean);
 		ResultSet rs = stmt.executeQuery();
 
