65d6a47a4f303b0bb9d3614ead11afef8a0e3507
Ross Kelso <rosskelso@btinternet.com>
2017-01-03 12:04:04 +0000

    Addded a debug statement

--- src/main/java/server/Request.java
+++ src/main/java/server/Request.java
@@ -101,6 +101,7 @@ public class Request implements Runnable {
 	}
 
 	public static String getEan(ArrayList<String> lines) {
+		System.out.println(lines.get(0));
 		// get the ean out of the request
 		int index1 = lines.get(0).indexOf('/') + 1;
 		int index2 = lines.get(0).lastIndexOf(' ');
