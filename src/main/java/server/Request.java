6a4bdefb9b790aac6e7eec0be12ed940fd054ecf
Ross Kelso <rosskelso@btinternet.com>
2017-01-05 14:14:10 +0000

    Added print statements for debugging

--- src/main/java/server/Request.java
+++ src/main/java/server/Request.java
@@ -15,6 +15,7 @@ public class Request implements Runnable {
 
 	public Request(Socket client) {
 		this.client = client;
+		System.out.println("New Request Created");
 	}
 
 	@Override
@@ -26,38 +27,46 @@ public class Request implements Runnable {
 
 			// send the client our headers
 			headers(out);
+			System.out.println("Our headers sent");
 
 			// load in the header data
 			ArrayList<String> lines = readHeaders(in);
 
+			System.out.println("Getting the EAN");
 			String ean = getEan(lines);
-
+			System.out.println("Getting the Method");
 			String method = getMethod(lines);
 
 			// declare a new database manager
 			DatabaseManager dbm = new DatabaseManager(url, user, pass);
 
 			if (method.equalsIgnoreCase("get")) {
+				System.out.println("This is a GET request");
 				// send the response to the client
 				out.print(dbm.getJSON(ean) + "\r\n");
 				System.out.println("Returned data on: " + ean);
 			} else if (method.equalsIgnoreCase("post")) {
 				// load in each new line
+				System.out.println("This is a POST request");
 				String line;
+				System.out.println("Reading data lines");
 				while ((line = in.readLine()) != null) {
+					System.out.println("Reading line; " + line);
 					lines.add(line);
 				}
 				// really this should probably parsing some json
+				System.out.println("Parsing JSON on line: " + lines.get(lines.size() - 1));
 				int[] data = JSONify.fromJSON(lines.get(lines.size() - 1));
 
 				boolean exists = dbm.exists(ean) /*|| Record.hasRecord(ean)*/;
-
 				// update the row if it already exists
 				if (exists) {
+					System.out.println("Record already exists");
 					//Record.update(ean, data, dbm);
 					dbm.update(ean, data);
 					System.out.println("Set " + ean + " to " + data);
 				} else {
+					System.out.println("Record doesn't already exist");
 					//Record.add(ean, data);
 					dbm.add(ean, data);
 					System.out.println("Added " + ean + " and set to " + data);
@@ -83,15 +92,18 @@ public class Request implements Runnable {
 	}
 
 	public static ArrayList<String> readHeaders(BufferedReader in) throws IOException {
+		System.out.println("Reading their headers");
 		// read the headers the client sends into an arraylist
 		ArrayList<String> lines = new ArrayList<String>();
 		String line;
 		while ((line = in.readLine()) != null) {
+			System.out.println("Reading line: " + line);
 			if (line.length() == 0) {
 				break;
 			}
 			lines.add(line);
 		}
+		System.out.println("Finished Reading Headers");
 
 		return lines;
 	}
